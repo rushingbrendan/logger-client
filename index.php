be36e8c726c62e1a50bab1a43b81038ef51e75f4
Brendan.Rushing <Brendan.Rushing@peergroup.com>
2020-11-15 15:59:13 -0500

    use ENV variables

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
 
+# Enviroment variables
 $client_guid = getenv('CLIENT_GUID');
 $server_url = getenv('SERVER_URL');
 
-//include_once("test.html")
-
 ?>
+
+<!-- 
+  FILE          : index.php
+  PROJECT       : SENG2040 - Network Application Development - Logging Service
+  PROGRAMMER    : Brendan Rushing & Eunjune Wi
+  FIRST VERSION : 2020-11-15
+  DESCRIPTION   :   Logging client to test logging service. 
+                    Client made in php
+                    Server made in phython
+                    Client and Server hosted in Heroku and Enviroment variables are used to store senstive information.
+-->
 
 <html>
 <head>
@@ -16,105 +26,105 @@ $server_url = getenv('SERVER_URL');
 <body>
 <script type="text/javascript">
 
+/*
+FUNCTION :BuildMessage
+DESCRIPTION : This function builds the logging message to be sent to server.
+PARAMETERS : level, message
+RETURNS : message
+*/
 function BuildMessage(level, message){
+
+    // Get token from enviroment variables.
     var token = "<?php echo $client_guid ?>";
-    console.log(token);
-
+    
+    // Build JSON.
     var toSend = { Level: level, AuthGuid: token, Message: message };
-
     return toSend;
 }
+
 $(document).ready(function() {
+
+    // Get server URL from enviroment variables.
     var server = "<?php echo $server_url ?>";
 
+    // Create web socket
 	var socket = io.connect(server);
 
+    // Connect web socket.
 	socket.on('connect', function() {
 	});
 
+    // Message has been received.
 	socket.on('message', function(msg) {
 		$("#messages").append('<li>Received message: '+msg+'</li>');
 	});
 
+    // Debug button pressed.
 	$('#debugSendbutton').on('click', function() {		
-        var toSend = BuildMessage("Debug", $('#myMessage').val() )
+        var toSend = BuildMessage("Debug", $('#myMessage').val())
 		socket.send(JSON.stringify(toSend));
 	});
 	
-	$('#InvalidGuidSend').on('click', function() {
-		var toSend = { Level: "Debug", AuthGuid: "nah", Message: $('#myMessage').val() };
-		socket.send(JSON.stringify(toSend));
-	});
-	
+    // Critical button pressed.
 	$('#criticalSendbutton').on('click', function() {
-		var toSend = { Level: "Critical", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val() };
+        var toSend = BuildMessage("Critical", $('#myMessage').val())
 		socket.send(JSON.stringify(toSend));
 	});
 	
+    // Error button pressed.
 	$('#errorSendbutton').on('click', function() {
-		var toSend = { Level: "Error", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val() };
+        var toSend = BuildMessage("Error", $('#myMessage').val())
 		socket.send(JSON.stringify(toSend));
 	});
 
+    // Warning button pressed.
 	$('#warningSendbutton').on('click', function() {
-		var toSend = { Level: "Warning", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val() };
+        var toSend = BuildMessage("Warning", $('#myMessage').val())
 		socket.send(JSON.stringify(toSend));
 	});
 
+    // Info button pressed.
 	$('#infoSendbutton').on('click', function() {
-		var toSend = { Level: "Info", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val() };
+        var toSend = BuildMessage("Info", $('#myMessage').val())
 		socket.send(JSON.stringify(toSend));
 	});
 
+    // Automated testing button pressed.
 	$('#massSendbutton').on('click', function() {
-		var toSend = { Level: "Critical", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val() };
-		socket.send(JSON.stringify(toSend));
-		toSend = { Level: "Critical", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0",Message: $('#myMessage').val() };
-		socket.send(JSON.stringify(toSend));
-		toSend = { Level: "Critical", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val() };
-		socket.send(JSON.stringify(toSend));
-		
-		toSend = { Level: "Error", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val() };
-		socket.send(JSON.stringify(toSend));
-		toSend = { Level: "Error", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val()};
-		socket.send(JSON.stringify(toSend));
-		toSend = { Level: "Error", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val()};
-		socket.send(JSON.stringify(toSend));
-		
-		toSend = { Level: "Warning", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val()};
-		socket.send(JSON.stringify(toSend));
-		toSend = { Level: "Warning", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val()};
-		socket.send(JSON.stringify(toSend));
-		toSend = { Level: "Warning", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0",Message: $('#myMessage').val()};
-		socket.send(JSON.stringify(toSend));
-		
-		toSend = { Level: "Info", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val()};
-		socket.send(JSON.stringify(toSend));
-		toSend = { Level: "Info", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val()};
-		socket.send(JSON.stringify(toSend));
-		toSend = { Level: "Info", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val() };
-		socket.send(JSON.stringify(toSend));
-		
-		toSend = { Level: "Debug", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val() };
-		socket.send(JSON.stringify(toSend));
-		toSend = { Level: "Debug", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val()};
-		socket.send(JSON.stringify(toSend));
-		toSend = { Level: "Debug", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: $('#myMessage').val()};
-		socket.send(JSON.stringify(toSend));
+        for (var y = 0; y < 200; y++){
+            var toSend = BuildMessage("Critical", $('#myMessage').val())        
+		    socket.send(JSON.stringify(toSend));
+        }        
 	});
 	
+    // No level button pressed.
 	$('#NoLevel').on('click', function() {
-		var toSend = { AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0", Message: "No Level" };
+
+        // Get token from enviroment variables.
+        var token = "<?php echo $client_guid ?>";
+
+		var toSend = { AuthGuid: token, Message: "No Level" };
 		socket.send(JSON.stringify(toSend));
 	});
 	
+    // No auth button pressed.
 	$('#NoAuth').on('click', function() {
 		var toSend = { Level: "Debug", Message: "No Auth" };
 		socket.send(JSON.stringify(toSend));
 	});
 	
+    // No message button pressed.
 	$('#NoMessage').on('click', function() {
-		var toSend = { Level: "Debug", AuthGuid: "878100a5-3228-4fbe-b91e-1e61a888f4e0"};
+
+        // Get token from enviroment variables.
+        var token = "<?php echo $client_guid ?>";
+		var toSend = { Level: "Debug", AuthGuid: token};
+		socket.send(JSON.stringify(toSend));
+	});
+
+    // Invalid Guid button pressed.
+	$('#InvalidGuidSend').on('click', function() {
+		var toSend = { Level: "Debug", AuthGuid: "nah", Message: $('#myMessage').val() };
 		socket.send(JSON.stringify(toSend));
 	});
 
@@ -127,14 +137,12 @@ $(document).ready(function() {
 <button id="warningSendbutton">Warning</button>
 <button id="infoSendbutton">Info</button>
 <button id="debugSendbutton">Debug</button>
-<button id="massSendbutton">Mass AutoTesting</button>
 <button id="InvalidGuidSend">Invalid Guid</button>
 <button id="NoLevel">No Level</button>
 <button id="NoAuth">No Auth</button>
 <button id="NoMessage">No Message</button>
-<!--https://mysterious-woodland-76957.herokuapp.com-->
-<!--https://localhost:5000-->
-<h1>Server: <?php echo "$server_url" ?> </h1>
-<h1>Guid: <?php echo "$client_guid" ?> </h1>
+
+
+<button id="massSendbutton">Mass AutoTesting</button>
 </body>
 </html>
